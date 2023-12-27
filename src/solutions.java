ddf2c666fcbfacba481219239f363c8dd159dfc5
Kazi Hasan Al Banna <130343033+hasanalBnna@users.noreply.github.com>
2023-12-28 00:09:20 +0600

    solutions.java

--- src/solutions.java
+++ src/solutions.java
@@ -3,6 +3,8 @@ public class solutions {
     public static void main(String[] args) {
         int[] ara= {1,2,30,4,50};
         System.out.println(sum(ara));//1. Sum of Array Elements: Write a program to find the sum of all elements in an array.
+        System.out.println(getMax(ara));//2. Largest Element in Array: Find and print the largest element in an array.
+        
 }
     public static int sum(int[] ara){
         int sum = 0;
@@ -11,6 +13,15 @@ public class solutions {
             sum= sum+ara[i];
         }
         return sum;
+    }
+    public static int getMax(int[] ara){
+        int max = ara[0];
+        for(int i=0;i<ara.length;i++){
+            if(ara[i] > max){
+                max = ara[i];
+            }
+        }
+        return max;
     }
 }
 
