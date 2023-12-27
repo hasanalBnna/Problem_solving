ad275b6bd74d4cbe14cc3944e4b95c06dd49b7b3
Kazi Hasan Al Banna <130343033+hasanalBnna@users.noreply.github.com>
2023-12-27 23:47:48 +0600

    solutions.java

--- src/solutions.java
+++ src/solutions.java
@@ -1,2 +1,15 @@
 public class solutions {
+    public static void main(String[] args) {
+        int[] ara= {1,2,30,4,50};
+        System.out.println(sum(ara));
 }
+    public static int sum(int[] ara){
+        int sum = 0;
+        int i;
+        for(i = 0; i< ara.length; i++){
+            sum= sum+ara[i];
+        }
+        return sum;
+    }
+}
+
