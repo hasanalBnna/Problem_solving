6e78ff48f4c5e8067ff44571ab01204b05f96de3
Kazi Hasan Al Banna <130343033+hasanalBnna@users.noreply.github.com>
2023-12-28 13:35:16 +0600

    solutions.java

--- src/solutions.java
+++ src/solutions.java
@@ -4,7 +4,13 @@ public class solutions {
         int[] ara= {1,2,30,4,50};
         System.out.println(sum(ara));//1. Sum of Array Elements: Write a program to find the sum of all elements in an array.
         System.out.println(getMax(ara));//2. Largest Element in Array: Find and print the largest element in an array.
-        
+        for (int i = 0; i < ara.length; i++) {//3. Even or Odd Numbers: Check if a given number is even or odd using if-else. (also count in an array)
+            if (ara[i] % 2 == 0) {
+                System.out.println(ara[i] + " is even");
+            } else
+                System.out.println(ara[i] + " is odd");
+        }
+        reverse(ara);//4. Reverse an Array: Reverse the elements of an array.
 }
     public static int sum(int[] ara){
         int sum = 0;
@@ -22,6 +28,11 @@ public class solutions {
             }
         }
         return max;
+    }
+    public static void reverse(int[] ara) {
+        for(int i=ara.length-1;i>=0;i--) {
+            System.out.print(ara[i] + " ");
+        }
     }
 }
 
